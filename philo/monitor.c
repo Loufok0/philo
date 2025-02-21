3ea375b2f3347db1645a735aa622445e8327f2ea
l <maelapouge@gmail.com>
2025-02-21 22:52:53 +0100

    endeeeeeeeeedd, just make sure the problem with valgrind isn't important

---
+++ philo/monitor.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   monitor.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malapoug <malapoug@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/21 18:00:28 by malapoug          #+#    #+#             */
+/*   Updated: 2025/02/21 18:02:31 by malapoug         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	loop_check(t_philo *philo, int *eaten)
+{
+	t_philosopher		*curr;
+	long int			current_time;
+
+	curr = (t_philosopher *)(philo->head);
+	while (curr)
+	{
+		current_time = get_timestamp(philo);
+		pthread_mutex_lock(&(curr->data_m));
+		if (current_time - curr->last_t_eat > philo->t_die)
+		{
+			tell("%ld\tPhilosopher %d \t\033[31mdied\033[0m\n"\
+				, philo, curr->id, -1);
+			pthread_mutex_lock(&(philo->stop_m));
+			philo->stop = 1;
+			pthread_mutex_unlock(&(philo->stop_m));
+			pthread_mutex_unlock(&(curr->data_m));
+			return (0);
+		}
+		if (curr->times_eaten < philo->n_eat)
+			*(eaten) = 0;
+		pthread_mutex_unlock(&(curr->data_m));
+		curr = curr->next;
+	}
+	return (1);
+}
+
+void	*monitor(void *arg)
+{
+	t_philo				*philo;
+	int					eaten;
+
+	philo = (t_philo *)arg;
+	while (1)
+	{
+		eaten = 1;
+		if (!loop_check(philo, &eaten))
+			return (NULL);
+		if (eaten == 1)
+		{
+			tell(GREEN"%ld\tAll Philosophers have eaten at least %lld times \n"\
+				RESET, philo, -1, philo->n_eat);
+			pthread_mutex_lock(&(philo->stop_m));
+			philo->stop = 1;
+			pthread_mutex_unlock(&(philo->stop_m));
+			return (NULL);
+		}
+		tempo(philo, 1);
+	}
+	return (NULL);
+}
--- philo/process.c
+++ philo/process.c
@@ -6,54 +6,11 @@
 /*   By: malapoug <malapoug@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 15:12:41 by malapoug          #+#    #+#             */
-/*   Updated: 2025/02/21 01:41:06 by malapoug         ###   ########.fr       */
+/*   Updated: 2025/02/21 18:00:20 by malapoug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-void	*monitor(void *arg)
-{
-	t_philo				*philo;
-	t_philosopher		*curr;
-	long int			current_time;
-	int					eaten;
-
-	philo = (t_philo *)arg;
-	while (1)
-	{
-		eaten = 1;
-		curr = (t_philosopher *)(philo->head);
-		while (curr)
-		{
-			current_time = get_timestamp(philo);
-			pthread_mutex_lock(&(curr->data_m));
-			if (current_time - curr->last_t_eat > philo->t_die)
-			{
-				tell("%ld\tPhilosopher %d \t\033[31mdied\033[0m\n", philo, curr->id, -1);
-				pthread_mutex_lock(&(philo->stop_m));
-				philo->stop = 1;
-				pthread_mutex_unlock(&(philo->stop_m));
-				pthread_mutex_unlock(&(curr->data_m));
-				return (NULL);
-			}
-			if (curr->times_eaten < philo->n_eat)
-				eaten = 0;
-			pthread_mutex_unlock(&(curr->data_m));
-			curr = curr->next;
-		}
-		if (eaten == 1)
-		{
-			tell(GREEN"%ld\tAll Philosophers have eaten at least %lld times \n"RESET, philo, -1, philo->n_eat);
-			pthread_mutex_lock(&(philo->stop_m));
-			philo->stop = 1;
-			pthread_mutex_unlock(&(philo->stop_m));
-			return (NULL);
-		}
-		tempo(philo, 1);
-	}
-	return (NULL);
-}
 
 int	check_stop(t_philo *philo)
 {
@@ -67,31 +24,39 @@ int	check_stop(t_philo *philo)
 	return (0);
 }
 
+void	update_data(t_philosopher *philo)
+{
+	pthread_mutex_lock(&(philo->data_m));
+	philo->last_t_eat = get_timestamp(philo->philo);
+	philo->times_eaten++;
+	pthread_mutex_unlock(&(philo->data_m));
+}
+
 int	actions(t_philosopher *philo)
 {
 	if (check_stop(philo->philo) || lock_mutex(philo) != 2)
 		return (0);
-	tell("%ld\tPhilosopher %d \tis \033[32meating\033[0m his meal n°%lld\n", philo->philo, philo->id, philo->times_eaten + 1);
+	tell("%ld\tPhilosopher %d \tis \033[32meating\033[0m his meal n°%lld\n"\
+		, philo->philo, philo->id, philo->times_eaten + 1);
 	if (check_stop(philo->philo))
 	{
 		pthread_mutex_unlock(&(philo->fork));
 		pthread_mutex_unlock(&(philo->prev->fork));
 		return (0);
 	}
-	pthread_mutex_lock(&(philo->data_m));
-	philo->last_t_eat = get_timestamp(philo->philo);
-	philo->times_eaten++;
-	pthread_mutex_unlock(&(philo->data_m));
+	update_data(philo);
 	tempo(philo->philo, philo->philo->t_eat);
 	pthread_mutex_unlock(&(philo->fork));
 	pthread_mutex_unlock(&(philo->prev->fork));
 	if (check_stop(philo->philo))
 		return (0);
-	tell("%ld\tPhilosopher %d \tis \033[35msleeping\033[0m\n", philo->philo, philo->id, -1);
+	tell("%ld\tPhilosopher %d \tis \033[35msleeping\033[0m\n"\
+		, philo->philo, philo->id, -1);
 	tempo(philo->philo, philo->philo->t_sleep);
 	if (check_stop(philo->philo))
 		return (0);
-	tell("%ld\tPhilosopher %d \tis \033[33mthinking\033[0m\n", philo->philo, philo->id, -1);
+	tell("%ld\tPhilosopher %d \tis \033[33mthinking\033[0m\n"\
+		, philo->philo, philo->id, -1);
 	return (1);
 }
 
